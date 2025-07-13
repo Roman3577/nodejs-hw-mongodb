2606423ee7d5e5c9b8cade3765a5efca3dd0dea0
Roma3577 <mularr37@gmail.com>
2025-07-13 13:50:22 +0300

    done5

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,21 +1,19 @@
 import mongoose from 'mongoose';
 
-const contactSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
-    email: { type: String },
-    isFavourite: { type: Boolean, default: false },
-    contactType: {
-      type: String,
-      enum: ['work', 'home', 'personal'],
-      default: 'personal',
-      required: true,
-    },
+const contactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  email: { type: String },
+  isFavourite: { type: Boolean, default: false },
+  contactType: {
+    type: String,
+    enum: ['work', 'home', 'personal'],
+    default: 'personal',
+    required: true,
   },
-  { timestamps: true }
-);
+}, { timestamps: true });
 
-const Contact = mongoose.model('Contact', contactSchema);
+
+const Contact = mongoose.model('Contact', contactSchema, 'students');
 
 export default Contact;
--- src/server.js
+++ src/server.js
@@ -12,8 +12,14 @@ const setupServer = () => {
   app.get('/contacts', getAll);
   app.get('/contacts/:contactId', getById);
 
+
   app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
+  });
+
+  app.use((err, req, res, next) => {
+    console.error('Error handler:', err);
+    res.status(500).json({ message: err.message || 'Internal Server Error' });
   });
 
   const PORT = process.env.PORT || 3000;
